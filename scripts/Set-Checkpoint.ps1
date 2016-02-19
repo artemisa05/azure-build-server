961035cc3111a679a363b4339a5a79a64cbbf577
Tim Murphy <tim@26tp.com>
2016-02-20 09:05:50 +1100

    Add logging when saving checkpoint

--- scripts/Set-Checkpoint.ps1
+++ scripts/Set-Checkpoint.ps1
@@ -13,5 +13,14 @@ Import-Module $PSScriptRoot\Library\Library.psm1 -Force
 
 $config = Read-Config
 
+Write-Host "Getting virtual machine '$($config.azure.virtualMachine.name)'..."
 $vm = Get-AzureVM -Name $config.azure.virtualMachine.name -ServiceName $config.azure.service.name
+
+if ($vm -eq $null) {
+    throw "Could not find virtual machine '$($config.azure.virtualMachine.name)'..."
+}
+
+Write-Host "Saving checkpoint '$Name'..."
 Set-AzureVMCheckpoint -VM $vm -CheckpointName $Name | Out-Null
+
+Write-Host "Successfully created checkpoint '$Name'."
